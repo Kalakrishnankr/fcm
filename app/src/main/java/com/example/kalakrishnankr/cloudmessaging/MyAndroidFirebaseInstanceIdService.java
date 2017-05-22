c64c3651a2566b21e76976a0bb9ba27d57fa866b
Kalakrishnan <kalakrishnan.kr@qagate.com>
2017-05-22 17:00:01 +0530

    Add existing file

---
+++ app/src/main/java/com/example/kalakrishnankr/cloudmessaging/MyAndroidFirebaseInstanceIdService.java
@@ -0,0 +1,25 @@
+package com.example.kalakrishnankr.cloudmessaging;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by kalakrishnan.kr on 22/5/17.
+ */
+public class MyAndroidFirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyAndroidFCMIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+        //Get hold of the registration token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        //Log the token
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+    }
+    private void sendRegistrationToServer(String token) {
+        //Implement this method if you want to store the token on your server
+    }
+}
